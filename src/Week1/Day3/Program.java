f4df46ad6cebeaa22cce7b092658fc8ce234573a
Emmanuel <ecordero8192@live.com>
2019-06-27 12:51:09 -0400

    Finished day 3 homeworks

--- src/Week1/Day3/Program.java
+++ src/Week1/Day3/Program.java
@@ -5,10 +5,14 @@ import java.io.*;
 
 public class Program {
 
+	final String PATH = "src/Week1/Day3/test.txt";
+	
 	public static void main(String[] args) {
 		Program prog = new Program();
 		
-		prog.getDirectory();
+		//prog.getDirectory();
+		//prog.appendText();
+		prog.charCount();
 		
 	}
 	
@@ -28,9 +32,66 @@ public class Program {
 		}
 		else {
 			System.out.println("There is something wrong with that path");
+		}		
+		scanner.close();		
+	}
+	
+	
+	public void appendText() {
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What text do you want to append to test.txt?");
+		String text = scanner.nextLine();
+		
+		File file = new File(PATH);
+		try {		
+			if(file.exists()) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+				writer.write(text);
+				writer.close();				
+			}
+			else {
+				System.out.println(file.getAbsolutePath());
+				System.out.println("That file doesnt exist");
+			}
+		}
+		catch(Exception ex) {			
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+		finally {
+			scanner.close();
+		}					
+	}
+	
+	
+	public void charCount() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the char you would like to count in the file");
+		char theChar = scanner.next().charAt(0);
+		scanner.close();
+		File file = new File(PATH);
+		String text = "";
+		try {
+			scanner = new Scanner(file);
+			
+			while(scanner.hasNext()) {
+				text += scanner.next();
+			}
+			
+			int count = 0;
+			for(char letter : text.toCharArray()) {
+				if(letter == theChar) {
+					count++;
+				}
+			}			
+			scanner.close();	
+			
+			System.out.println("The char " + theChar + " appears " + count + " amount of times");
+		} catch (Exception e) {			
+			e.printStackTrace();
 		}
 		
-		scanner.close();
 		
 	}
 	
